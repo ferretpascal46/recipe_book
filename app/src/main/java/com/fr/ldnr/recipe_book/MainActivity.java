85fd86734e9a41c2462f1a97bc70f9c50a182519
MonnierM <monniermichel81@gmail.com>
2020-07-21 18:59:46 +0200

    Test commit 1

--- app/src/main/java/com/fr/ldnr/recipe_book/MainActivity.java
+++ app/src/main/java/com/fr/ldnr/recipe_book/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("TEST", "Pascal test ");
+        Log.d("TEST", "Michel test ");
     }
 }
