833105856f698ad0388b675e2a3d21ec93f58ca4
PascalFerret <ferretpascal46@gmail.com>
2020-07-23 16:04:53 +0200

    modifications des balises

--- app/src/main/java/com/fr/ldnr/recipe_book/dao/DAORecipe.java
+++ app/src/main/java/com/fr/ldnr/recipe_book/dao/DAORecipe.java
@@ -94,11 +94,11 @@ public class DAORecipe {
 
         while (result.moveToNext()) {
             recipes.add(new RecipeObject(
-                    result.getInt(1),
+                    result.getInt(0),
+                    result.getString(1),
                     result.getString(2),
                     result.getString(3),
-                    result.getString(4),
-                    result.getString(5))
+                    result.getString(4))
             );
         }
 
@@ -122,11 +122,11 @@ public class DAORecipe {
         while (result.moveToNext()) {
             recipes.add(
                     new RecipeObject(
-                            result.getInt(1),
+                            result.getInt(0),
+                            result.getString(1),
                             result.getString(2),
                             result.getString(3),
-                            result.getString(4),
-                            result.getString(5))
+                            result.getString(4))
             );
         }
 
@@ -166,11 +166,11 @@ public class DAORecipe {
         while (result.moveToNext()) {
             recipes.add(
                     new RecipeObject(
-                            result.getInt(1),
+                            result.getInt(0),
+                            result.getString(1),
                             result.getString(2),
                             result.getString(3),
-                            result.getString(4),
-                            result.getString(5))
+                            result.getString(4))
             );
         }
 
